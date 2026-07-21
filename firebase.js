aeb1b472f5cb88b82048b4f89fcc6fc911929f22
hamsikajai <hamsikajai@gmail.com>
2026-07-21 11:17:13 +0530

    Fix export statement in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
