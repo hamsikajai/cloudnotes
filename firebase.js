acb3c27eec76ba666a64983eb8cdec44c2effea9
hamsikajai <hamsikajai@gmail.com>
2026-07-21 10:41:56 +0530

    Add Firebase Auth and Database exports

--- firebase.js
+++ firebase.js
@@ -1,7 +1,9 @@
-<script type="module">
+
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+  import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,4 +32,4 @@ if (typeof window !== "undefined") {
 const auth = getAuth(app);
 const database = getDatabase(app);
 
-export { auth, database };
+export { app, auth, database };
