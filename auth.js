a5ae279fa0048b09f8666b951048a0c12edb1dcf
hamsikajai <hamsikajai@gmail.com>
2026-07-21 11:15:41 +0530

    Implement Google and email authentication

--- auth.js
+++ auth.js
@@ -1,1 +1,74 @@
-alert("auth.js loaded");
+
+import { auth } from "./firebase.js";
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const googleBtn = document.getElementById("googleSignIn");
+    const loginBtn = document.getElementById("loginBtn");
+    const signupBtn = document.getElementById("signupBtn");
+
+    const provider = new GoogleAuthProvider();
+
+    googleBtn.addEventListener("click", async () => {
+
+        try {
+
+            await signInWithPopup(auth, provider);
+
+            window.location.href = "dashboard.html";
+
+        } catch (error) {
+
+            alert(error.message);
+
+        }
+
+    });
+
+    loginBtn.addEventListener("click", async () => {
+
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value;
+
+        try {
+
+            await signInWithEmailAndPassword(auth, email, password);
+
+            window.location.href = "dashboard.html";
+
+        } catch (error) {
+
+            alert(error.message);
+
+        }
+
+    });
+
+    signupBtn.addEventListener("click", async () => {
+
+        const email = document.getElementById("email").value.trim();
+        const password = document.getElementById("password").value;
+
+        try {
+
+            await createUserWithEmailAndPassword(auth, email, password);
+
+            window.location.href = "dashboard.html";
+
+        } catch (error) {
+
+            alert(error.message);
+
+        }
+
+    });
+
+});
+
